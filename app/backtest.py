2c140b9fb7a7b911de6d3cd528c4a7a3aaa71da6
longmfe <longmfe@gmail.com>
2024-04-14 09:33:52 +0000

    add function for get_hist_data_by_count

--- app/backtest.py
+++ app/backtest.py
@@ -2,19 +2,45 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from xtquant import xtdata
+import datetime
+import dateutil
 
 #TODO: what if the portfolio inclue equity across the market
 market = 'SH'
 
 class Context:
-    def __init__(self, market, cash, start_date, end_date):
+    def __init__(self, market, cash, bkt_start_date, bkt_end_date):
+        self.market = market
         self.cash = cash
-        self.start_date = start_date
-        self.end_date = end_date
+        self.bkt_start_date = bkt_start_date
+        self.bkt_end_date = bkt_end_date
         self.positions = {}
         self.benchmark = None
-        self.date_range = xtdata.get_trading_calendar(market, start_date, end_date)
+        self.date_range = xtdata.get_trading_calendar(market, bkt_start_date, bkt_end_date)
+        self.bkt_dt = dateutil.parser.parse(bkt_start_date)
 
-context = Context(1000, 201601, 201701)
+context = Context(market, 1000, '20160101', '20170101')
 print(context.date_range)
+
+def get_hist_data_by_count(code, count, fields=['open', 'close', 'high', 'low', 'volume', 'preClose']):
+    hist_end_date = (context.bkt_dt - datetime.timedelta(days=1)).strftime("%Y%m%d")
+
+    trd_calendar = xtdata.get_trading_calendar(context.market)
+    hist_start_date = [dt for dt in trd_calendar if dt <= hist_end_date][-count:][0]
+
+    _period = '1d'
+    code = code+"."+context.market
+    xtdata.download_history_data(code, _period, hist_start, hist_end)
+
+    ret = xtdata.get_market_data_ex(field_list=fields,
+                                    stock_list=[code],
+                                    period=_period,
+                                    start_time='',
+                                    end_time=hist_end_date,
+                                    count=count
+            )
+
+    return ret
+
+#def get_hist_data_by_count(code, hist_start, hist_end, fields=['open', 'close', 'high', 'low', 'volume', 'preClose']):
 
