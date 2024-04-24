f4e8f5c3e38a832930f12732eca673c14bcd2105
longmfe <longmfe@gmail.com>
2024-04-24 07:32:38 +0000

    add function order

--- app/backtest.py
+++ app/backtest.py
@@ -42,5 +42,52 @@ def get_hist_data_by_count(code, count, fields=['open', 'close', 'high', 'low', 
 
     return ret
 
-#def get_hist_data_by_count(code, hist_start, hist_end, fields=['open', 'close', 'high', 'low', 'volume', 'preClose']):
+def get_today_data(code, fields=['open', 'close', 'high', 'low', 'volume', 'preClose']):
+    _period = '1d'
+    _count = 1
+    code = code+"."+context.market
+    today = context.date_range[0]
+    xtdata.download_history_data(code, _period, today, today)
+
+    try:
+        ret = xtdata.get_market_data_ex(field_list=fields,
+                stock_list=[code],
+                period=_period,
+                start_time='',
+                end_time=today,
+                count=_count
+                )[code]
+    except KeyError:
+        ret = pd.Series()
+
+    return ret
+
+def _order(code, amount):
+    today_data = get_today_data(code)
+    if len(today_data) == 0:
+        print('no trading today')
+        return
+
+    price = today_data['close'].values[0]
+    if context.cash - amount * price < 0:
+        amount = int(context.cash / price)
+        print('not enough cash, adjusted to %d' %(amount))
+
+    if amount % 100 != 0:
+        #if sold all you have, not need to adjust
+        if amount != -context.positions.get(code, 0):
+            amount = int(amount / 100) * 100
+            print('adjusted to buyable amount %d' % amount)
+
+    # adjust when selling all you have
+    if context.positions.get(code, 0) < -amount:
+        amount = context.positions.get(code, 0)
+        print('can not sell security exceding the account holding, adjusted to %d' % amount)
+
+    context.positions[code] = context.positions.get(code, 0) + amount
+    if context.positions[code] == 0:
+        del context.positions[code]
+
+    context.cash -= amount * price
+
 
